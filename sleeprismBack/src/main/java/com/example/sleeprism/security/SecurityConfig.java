be6d521b8c7c2816aeaf10fd36583e349a76cef9
hyoeunlike@naver.com <hyoeunlike@naver.com>
2025-07-11 18:04:46 +0900

    tarot card img

--- sleeprismBack/src/main/java/com/example/sleeprism/security/SecurityConfig.java
+++ sleeprismBack/src/main/java/com/example/sleeprism/security/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig {
             ).permitAll()
             .requestMatchers("/api/posts", "/api/posts/{postId}").permitAll()
             .requestMatchers("/files/profile-images/**", "/api/comments/post/**").permitAll()
-            .requestMatchers("/files/**").permitAll()
+            .requestMatchers("/files/**", "/images/**").permitAll()
             // 나머지 모든 요청은 인증을 요구합니다.
             .anyRequest().authenticated()
         )
